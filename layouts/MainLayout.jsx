518feb472f0fb2a18f37dead0fa5ba7771161438
Ivan <iv@Ivans-MacBook-Air.local>
2022-11-24 12:25:27 +0300

    add next font

--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -26,35 +26,6 @@ export const MainLayout = ({
         <meta name="description" content={metaDescription} />
         <meta name="keywords" content={keywords} />
         <meta property="og:image" content="/img/head/meta.jpg" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          rel="preload"
-          href="/fonts/Metropolis-Bold.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/Metropolis-Bold.woff"
-          as="font"
-          type="font/woff"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/Metropolis-Bold.ttf"
-          as="font"
-          type="font/ttf"
-          crossOrigin="anonymous"
-        />
-        <link rel="stylesheet" href="/fonts/style.css" />
       </Head>
       <Header
         transparent={headerTransparent}
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import "../styles/globals.scss";
 import NextNProgress from "nextjs-progressbar";
+import { Inter } from "@next/font/google";
+import localFont from "@next/font/local";
+const inter = Inter({ subsets: ["latin"] });
+const Metropolis = localFont({ src: "./fonts/Metropolis-Bold.woff2" });
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+          font-family: ${Metropolis.style.fontFamily};
+        }
+      `}</style>
       <NextNProgress
         color="#000"
         startPosition={0.3}
