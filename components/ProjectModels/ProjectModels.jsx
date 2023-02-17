d17a687433b2c540b06f0d58ac929c6410de4351
DXristianin <mits.bali@gmail.com>
2023-02-17 12:38:57 +0300

    Update ProjectModels.jsx

--- components/ProjectModels/ProjectModels.jsx
+++ components/ProjectModels/ProjectModels.jsx
@@ -14,6 +14,7 @@ const ProjectModels = ({ items }) => {
                 items.map((item) => (
                   <Grid key={item.id} item xs={12} md={4} lg={3}>
                     <Box className={styles.item}>
+                      {console.log(item.value)}
                       {item.value}
                     </Box>
                   </Grid>
