8f52fcde851a6d5c808486cb2902ca69b91b0baa
DXristianin <mits.bali@gmail.com>
2023-02-17 12:14:35 +0300

    Update ProjectModels.jsx

--- components/ProjectModels/ProjectModels.jsx
+++ components/ProjectModels/ProjectModels.jsx
@@ -9,17 +9,12 @@ const ProjectModels = ({ items }) => {
       <Container>
         <Grid container justifyContent={"center"}>
           <Grid item xs={12} lg={10}>
-            <h2 className={styles.title}>
-              BEST IN CLASS <br /> INVESTMENT <br /> OPPORTUNITY
-            </h2>
-
             <Grid container spacing={3}>
               {items &&
                 items.map((item) => (
                   <Grid key={item.id} item xs={12} md={4} lg={3}>
                     <Box className={styles.item}>
-                      <h3 className={styles.value}>{item.value}</h3>
-                      <p className={styles.description}>{item.description}</p>
+                      {item.value}
                     </Box>
                   </Grid>
                 ))}
