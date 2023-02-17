261bde93f031613eb5984b93326233b6659c9364
DXristianin <mits.bali@gmail.com>
2023-02-17 16:19:28 +0300

    Update ProjectModels.jsx

--- components/ProjectModels/ProjectModels.jsx
+++ components/ProjectModels/ProjectModels.jsx
@@ -14,15 +14,23 @@ const ProjectModels = ({ items }) => {
   return (
     <Box className={styles.projectModels}>
       <Container>
+        <Grid container justifyContent={"center"}>
+          <Grid item xs={12} lg={10}>
+            <Grid container spacing={3}>
               {items &&
                 items.map((item) => (
+                  <Grid key={item.id} item xs={12} md={4} lg={3}>
                     <Box  dangerouslySetInnerHTML={{__html: myModel(item.value)}}>
                       {/* {item.value.block?.data.html} */}
                       {/* {console.log(JSON.parse(item.value))} */}
                       {/* {console.log('123:',myModel(item.value))} */}
+                      
                     </Box>
+                  </Grid>
                 ))}
-         
+            </Grid>
+          </Grid>
+        </Grid>
       </Container>
     </Box>
   );
